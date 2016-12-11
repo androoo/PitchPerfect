3d322d4b19f4d17d03511eab9eb44e5a43a72bb1
Andrew Gierke <andrew.gierke@gmail.com>
2016-12-11 17:46:28 -0500

    Added all the audio delegation, fixed some constraint issues

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class RecordSoundsViewController: UIViewController {
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    //MARK: - Properties 
+    var audioRecorder: AVAudioRecorder!
 
     //MARK: - Outlets
     @IBOutlet weak var tapToRecordLabel: UILabel!
@@ -31,13 +35,99 @@ class RecordSoundsViewController: UIViewController {
         tapToRecordLabel.text = "Recording in progress..."
         stopRecordingButton.isEnabled = true
         recordButton.isEnabled = false
+        
+        //Start recording audio 
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        //check file path when start recording to check if things are working 
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
     }
     
     @IBAction func stopRecording(_ sender: AnyObject) {
         tapToRecordLabel.text = "Tap to record audio"
         recordButton.isEnabled = true
         stopRecordingButton.isEnabled = false
+        
+        //stop recording 
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     }
     
+    //when the avaudiorecorder is finished recording it calls for this func bc we set this vc as delegate
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("recording was not successful")
+        }
+    }
+    
+    //MARK: - seque 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "stopRecording"){
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
